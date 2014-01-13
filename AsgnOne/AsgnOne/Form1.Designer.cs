02d5b35ce21bacb5d1548547a1b628471ba83467
mward03 <matthewward@pcp077731pcs.wireless.calpoly.edu>
2014-01-13 15:29:37 -0800

    Inital commit for a partially complete asgn1, simply plays random portion each second

---
+++ AsgnOne/AsgnOne/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace AsgnOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open = new System.Windows.Forms.Button();
+            this.playPause = new System.Windows.Forms.Button();
+            this.gap = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(12, 12);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(397, 54);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open Wave";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // playPause
+            // 
+            this.playPause.Enabled = false;
+            this.playPause.Location = new System.Drawing.Point(12, 72);
+            this.playPause.Name = "playPause";
+            this.playPause.Size = new System.Drawing.Size(397, 52);
+            this.playPause.TabIndex = 1;
+            this.playPause.Text = "Play/Pause";
+            this.playPause.UseVisualStyleBackColor = true;
+            this.playPause.Click += new System.EventHandler(this.playPause_Click);
+            // 
+            // gap
+            // 
+            this.gap.AutoSize = true;
+            this.gap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gap.Location = new System.Drawing.Point(196, 226);
+            this.gap.Name = "gap";
+            this.gap.Size = new System.Drawing.Size(26, 28);
+            this.gap.TabIndex = 2;
+            this.gap.Text = "0";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 171);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(396, 52);
+            this.start.TabIndex = 3;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 281);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.gap);
+            this.Controls.Add(this.playPause);
+            this.Controls.Add(this.open);
+            this.Name = "Form1";
+            this.Text = "AsgnOne";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button playPause;
+        private System.Windows.Forms.Label gap;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ AsgnOne/AsgnOne/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.Threading;
+using System.Timers;
+
+namespace AsgnOne
+{
+    public partial class Form1 : Form
+    {
+        private  NAudio.Wave.WaveFileReader wave = null;
+        private NAudio.Wave.DirectSoundOut output = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            disposeWave();
+
+            wave = new NAudio.Wave.WaveFileReader(ofd.FileName);
+            output = new NAudio.Wave.DirectSoundOut();
+            output.Init(new NAudio.Wave.WaveChannel32(wave));
+
+            output.Play();
+
+            playPause.Enabled = true;
+            start.Enabled = true;
+        }
+
+        private void disposeWave()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Stop();
+                output.Dispose();
+                output = null;
+
+            }
+
+            if (wave != null)
+            {
+                wave.Dispose();
+                wave = null;
+            }
+        }
+
+        private void asgnOne_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            disposeWave();
+        }
+
+        private void playPause_Click(object sender, EventArgs e)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Pause();
+                else if (output.PlaybackState == NAudio.Wave.PlaybackState.Paused) output.Play();
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            
+         
+           
+            System.Timers.Timer testTimer = new System.Timers.Timer(1000);
+            testTimer.Elapsed += new ElapsedEventHandler(OnTimerElapsed);
+            testTimer.Enabled = true;
+
+        }
+
+
+        public void OnTimerElapsed(object source, ElapsedEventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            TimeSpan span = new TimeSpan(0, 0, 0, randomNumber, 0);
+            WaveStreamExtensions.SetPosition(wave, span); 
+        }
+    }
+}
+
+public static class WaveStreamExtensions
+{
+    // Set position of WaveStream to nearest block to supplied position
+    public static void SetPosition(this WaveStream strm, long position)
+    {
+        // distance from block boundary (may be 0)
+        long adj = position % strm.WaveFormat.BlockAlign;
+        // adjust position to boundary and clamp to valid range
+        long newPos = Math.Max(0, Math.Min(strm.Length, position - adj));
+        // set playback position
+        strm.Position = newPos;
+    }
+
+    // Set playback position of WaveStream by seconds
+    public static void SetPosition(this WaveStream strm, double seconds)
+    {
+        strm.SetPosition((long)(seconds * strm.WaveFormat.AverageBytesPerSecond));
+    }
+
+    // Set playback position of WaveStream by time (as a TimeSpan)
+    public static void SetPosition(this WaveStream strm, TimeSpan time)
+    {
+        strm.SetPosition(time.TotalSeconds);
+    }
+
+    // Set playback position of WaveStream relative to current position
+    public static void Seek(this WaveStream strm, double offset)
+    {
+        strm.SetPosition(strm.Position + (long)(offset * strm.WaveFormat.AverageBytesPerSecond));
+    }
+}
